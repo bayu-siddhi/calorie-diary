023a3cd587b57705a4a9f0a19b12eb6c8506dff1
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-06-14 17:51:27 +0700

    Add intent for Add Food UI

--- app/src/main/java/com/example/calorie_diary/AddFoodActivity.kt
+++ app/src/main/java/com/example/calorie_diary/AddFoodActivity.kt
@@ -1,12 +1,17 @@
 package com.example.calorie_diary
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AddFoodActivity : AppCompatActivity() {
+class AddFoodActivity : AppCompatActivity(), View.OnClickListener  {
+
+    private lateinit var backButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,12 @@ class AddFoodActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        backButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this@AddFoodActivity, SearchFoodActivity::class.java)
+        startActivity(intent)
     }
 }
